22cf41cc8aeae2ad29360267a0de1f277be46570
Ashwini Kumar Prince <joinprincee@gmail.com>
2021-04-14 14:12:27 +0530

    commit using slf4j

--- SpringBootTest/src/main/java/com/ashwini/SpringBootTest/component/MessageRunners.java
+++ SpringBootTest/src/main/java/com/ashwini/SpringBootTest/component/MessageRunners.java
@@ -1,16 +1,25 @@
 package com.ashwini.SpringBootTest.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageRunners implements CommandLineRunner {
 
+	private static Logger log = LoggerFactory.getLogger(MessageRunners.class);
+
 	@Override
 	public void run(String... args) throws Exception {
 		int a = 100;
-		System.out.println("-------- SpringBootTestApplication -------" + a);
-
+		try {
+			log.info("Started");
+			System.out.println("-------- SpringBootTestApplication -------" + a);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		log.info("END");
 	}
 
 }
